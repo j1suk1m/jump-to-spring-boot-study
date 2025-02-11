3ef008324ac2f8a607123c1b47b41a1fdcee775d
jisukimtech <jisukim.tech@gmail.com>
2025-02-12 02:27:02 +0900

    init: 프로젝트 생성

---
+++ src/test/java/study/springboot/board/SpringBootBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springboot.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
