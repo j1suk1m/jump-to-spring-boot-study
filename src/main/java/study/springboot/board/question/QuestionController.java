d299079da077ccbcdba1a6051859f4471bd4751f
jisukimtech <jisukim.tech@gmail.com>
2025-02-12 14:41:42 +0900

    feat: 질문 등록 폼 추가 및 유효성 검증

--- src/main/java/study/springboot/board/question/QuestionController.java
+++ src/main/java/study/springboot/board/question/QuestionController.java
@@ -1,14 +1,15 @@
 package study.springboot.board.question;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -32,13 +33,17 @@ public class QuestionController {
     }
 
     @GetMapping("/create")
-    public String createQuestion() {
+    public String createQuestion(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
-    public String createQuestion(@RequestParam String title, @RequestParam String content) {
-        questionService.createQuestion(title, content);
+    public String createQuestion(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        questionService.createQuestion(questionForm.getTitle(), questionForm.getContent());
         return "redirect:/question/list";
     }
 
