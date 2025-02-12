f39b08f99ed176b0ce22b5d757df127584dfa8ea
jisukimtech <jisukim.tech@gmail.com>
2025-02-12 19:07:37 +0900

    feat: 스프링 시큐리티 기본 설정
    - SecurityConfig 클래스 생성 및 스프링 시큐리티 활성화
    - 모든 요청에 대한 접근 허용
    - H2 콘솔 사용을 위한 CSRF 예외 처리 추가
    - X-Frame-Options SAMEORIGIN 설정

---
+++ src/main/java/study/springboot/board/SecurityConfig.java
@@ -0,0 +1,29 @@
+package study.springboot.board;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 스프링 시큐리티 활성화 -> 모든 요청 URL 제어
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()) // 인증되지 않은 모든 페이지의 요청 허용
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**"))) // /h2-console/로 시작하는 모든 URL의 CSRF 검증 제외
+                .headers(headers -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))); // X-Frame-Options 헤더를 SAMEORIGIN으로 설정
+
+        return http.build();
+    }
+
+}
