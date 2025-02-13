b91481da8e93749c2bf0d4dd0947bf5e72609462
jisukimtech <jisukim.tech@gmail.com>
2025-02-13 15:27:05 +0900

    feat: 답변 수정 기능 구현

--- src/main/java/study/springboot/board/answer/AnswerController.java
+++ src/main/java/study/springboot/board/answer/AnswerController.java
@@ -3,13 +3,16 @@ package study.springboot.board.answer;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import study.springboot.board.member.Member;
 import study.springboot.board.member.MemberService;
@@ -44,6 +47,49 @@ public class AnswerController {
 
         answerService.create(answerForm.getContent(), question, member);
         redirectAttributes.addAttribute("id", id);
+        return "redirect:/question/detail/{id}";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String modifyAnswer(
+            AnswerForm answerForm,
+            @PathVariable Integer id,
+            Principal principal
+    ) {
+        Answer answer = answerService.getAnswer(id);
+
+        if(!answer.getAuthor().getName().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String modifyAnswer(
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult,
+            @PathVariable Integer id,
+            Principal principal,
+            RedirectAttributes redirectAttributes
+    ) {
+        if (bindingResult.hasErrors()) {
+            return "answer_form";
+        }
+
+        Answer answer = answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getName().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+
+        answerService.modifyAnswer(answer, answerForm.getContent());
+        redirectAttributes.addAttribute("id", answer.getQuestion().getId());
+
         return "redirect:/question/detail/{id}";
     }
 
--- src/main/java/study/springboot/board/answer/AnswerService.java
+++ src/main/java/study/springboot/board/answer/AnswerService.java
@@ -1,7 +1,9 @@
 package study.springboot.board.answer;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import study.springboot.board.DataNotFoundException;
 import study.springboot.board.member.Member;
 import study.springboot.board.question.Question;
 
@@ -13,6 +15,21 @@ public class AnswerService {
 
     public void create(String content, Question question, Member author) {
         Answer answer = new Answer(content, question, author);
+        answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> optionalAnswer = answerRepository.findById(id);
+
+        if (optionalAnswer.isPresent()) {
+            return optionalAnswer.get();
+        }
+
+        throw new DataNotFoundException("Answer Not Found");
+    }
+
+    public void modifyAnswer(Answer answer, String content) {
+        answer.setContent(content);
         answerRepository.save(answer);
     }
 
